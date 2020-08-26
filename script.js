5d3727bd0da451063eb9bfbfa1a5481b39e055ab
Tanmay Garg <xtanmaygarg@gmail.com>
2020-08-27 00:44:12 +0530

    Changed Some Bugs

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ var player2Color = 'rgb(237, 45, 73)';
 var game_on = true;
 var table = $('table tr');
 
-// http://stackoverflow.com/questions/6139407/getting-td-by-index-with-jquery
 function reportWin(rowNum,colNum) {
   console.log("You won starting at this row,col");
   console.log(rowNum);
@@ -139,31 +138,3 @@ $('.board button').on('click',function() {
   }
 
 })
-
-
-
-// Helper function to help you understand Rows and Columns From A Table
-// http://stackoverflow.com/questions/788225/table-row-and-column-number-in-jquery
-//
-// $('.board button').on('click',function(){
-//   // This is the Column Number (starts at zero):
-//   console.log('This is the Column:');
-//   console.log($(this).closest("td").index());
-//   // This is the Row Number:
-//   console.log("This is the Row:");
-//   console.log($(this).closest("tr").index());
-//   console.log('\n');
-//   // This is a way to grab a particular cell (replace):
-//   // $('table').eq(rowIndex).find('td').eq(colIndex)
-// });
-
-// // Change color on click
-// $('.board button').on('click',function() {
-//   if($(this).css('background-color') === 'rgb(51, 51, 51)'){
-//     $(this).css('background-color','rgb(86, 151, 255)');
-//   }else if ($(this).css('background-color') === 'rgb(86, 151, 255)'){
-//     $(this).css('background-color','rgb(237, 45, 73)');
-//   }else{
-//     $(this).css('background-color','rgb(51, 51, 51)');
-//   }
-// });
